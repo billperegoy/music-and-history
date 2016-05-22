0e2a4cefbf206c91ae04be320b600610ec4fe7e5
Bill Peregoy <bill@billperegoy.com>
2016-05-22 19:48:10 -0400

    Added ability to display a single random event for the current day.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,17 @@
 class EventsController < ApplicationController
   def index
     @events = events_on_this_date_in_history
+    @random_events = random_event_on_this_date_in_history
   end
 
   private
   def events_on_this_date_in_history
     today = Date.today
     Event.by_month(today.month).by_day(today.day).order(date: :asc)
+  end
+
+  def random_event_on_this_date_in_history
+    today = Date.today
+    Event.by_month(today.month).by_day(today.day).order("RANDOM()").limit(1)
   end
 end
