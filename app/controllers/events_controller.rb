277d7ee140b13245382c99ba00242a6b23a43418
Bill Peregoy <bill@billperegoy.com>
2016-07-02 16:27:29 -0400

    Added visual elements for landing page.
    The CSS needs a lot of work but I wanted to snapshot before I had
    a chance to break things.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def index
     @events = events_on_this_date_in_history
-    @random_events = random_event_on_this_date_in_history
+    @random_event = random_event_on_this_date_in_history
   end
 
   private
@@ -12,6 +12,6 @@ class EventsController < ApplicationController
 
   def random_event_on_this_date_in_history
     today = Date.today
-    Event.by_month(today.month).by_day(today.day).order("RANDOM()").limit(1)
+    Event.by_month(today.month).by_day(today.day).order("RANDOM()").limit(1).first
   end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def todays_formatted_date_no_year
+    Time.now.strftime("%B %d")
+  end
 end
