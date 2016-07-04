b27f0a2969c2de0a88dbfdf073af8d6b6d9cf872
Bill Peregoy <bill@billperegoy.com>
2016-07-04 18:24:02 -0400

    Added support for random background image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def random_background_image
+    images = [
+      "background-Jason-James.png",
+      "background-Luz-Adriana-Villa.png",
+      "background-Leander-Arkenau.png",
+      "background-Michael-Mandiberg.png"
+    ]
+
+    image_num = rand(images.length)
+    "assets/#{images[image_num]}"
+  end
 end
