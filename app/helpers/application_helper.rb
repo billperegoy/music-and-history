2d9ce46c1395e322c5cd120e37931004ef3fcd5e
Annie Smidt <annie@durablecreative.com>
2016-07-15 18:15:37 -0400

    new background images, changes to pages styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,26 @@
 module ApplicationHelper
   def random_background_image
     images = [
-      "background-Jason-James.png",
-      "background-Luz-Adriana-Villa.png",
-      "background-Leander-Arkenau.png",
-      "background-Michael-Mandiberg.png"
+      "01-background.png",
+      "02-background.png",
+      "03-background.png",
+      "04-background.png",
+      "05-background.png",
+      "06-background.png",
+      "07-background.png",
+      "08-background.png",
+      "09-background.png",
+      "10-background.png",
+      "11-background.png",
+      "12-background.png",
+      "13-background.png",
+      "14-background.png",
+      "15-background.png",
+      "16-background.png",
+      "17-background.png",
+      "18-background.png",
+      "19-background.png",
+      "20-background.png"
     ]
 
     image_num = rand(images.length)
