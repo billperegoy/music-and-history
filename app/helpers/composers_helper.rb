1a5cb24287e5b13cb243d9a010558e5d6819e63c
Bill Peregoy <bill@billperegoy.com>
2016-07-16 10:44:54 -0400

    Fixed composer index page to not link empty composers.

--- app/helpers/composers_helper.rb
+++ app/helpers/composers_helper.rb
@@ -2,4 +2,12 @@ module ComposersHelper
   def full_name(composer)
     "#{composer.last_name}, #{composer.first_name}"
   end
+
+  def composer_link(composer)
+    if composer.events.any?
+      link_to full_name(composer), composer
+    else
+      full_name(composer)
+    end
+  end
 end
