0e74115dadc4e86187c8440d1e9d153bf7b345b9
Bill Peregoy <bill@billperegoy.com>
2016-07-23 22:01:05 -0400

    Moved first letter of composer name function to model.

--- app/models/composer.rb
+++ app/models/composer.rb
@@ -1,4 +1,8 @@
 class Composer < ActiveRecord::Base
   has_many :event_composer_connectors
   has_many :events, through: :event_composer_connectors
+
+  def first_letter_of_last_name
+    last_name[0,1]
+  end
 end
