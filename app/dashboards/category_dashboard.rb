bfb83a636e429c9b5544e08e2e7f4cc746837a0c
Bill Peregoy <bill@billperegoy.com>
2016-08-06 18:22:38 -0400

    More dashboard page cleanup.

--- app/dashboards/category_dashboard.rb
+++ app/dashboards/category_dashboard.rb
@@ -21,14 +21,12 @@ class CategoryDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :events,
     :name,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :events,
     :name,
   ].freeze
 
@@ -36,7 +34,6 @@ class CategoryDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :events,
     :name,
   ].freeze
 
