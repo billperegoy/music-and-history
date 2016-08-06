80c3d34e8d24f16cda8ecb7beff27b039b083529
Bill Peregoy <bill@billperegoy.com>
2016-08-05 23:12:02 -0400

    Fixed login issue.

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,6 @@
 Clearance.configure do |config|
   config.mailer_sender = "reply@example.com"
+  Clearance::PasswordsController.layout "sessions"
   Clearance::SessionsController.layout "sessions"
   Clearance::UsersController.layout "sessions"
 end
