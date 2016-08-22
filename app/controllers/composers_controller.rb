afd915abdab8fd179a93f51199de65255bc62766
Bill Peregoy <bill@billperegoy.com>
2016-08-22 17:31:19 -0400

    Fixed sorting of composers page.

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -1,7 +1,7 @@
 class ComposersController < ApplicationController
   def index
     @random_event = random_event_on_this_date_in_history
-    @composers = Composer.all
+    @composers = Composer.all.order(last_name: :asc, first_name: :asc)
   end
 
   def show
