20bbe790063abfad33d9db6f63f16b104016db49
Bill Peregoy <bill@billperegoy.com>
2016-08-22 18:48:45 -0400

    Fixed toggle button issue.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
   // Reset the checkbox if the page is reloaded
   $('#date-range-checkbox')[0].checked = false;
 
-  $( "#toggle-button" ).click(function(event) {
-    event.preventDefault();
+  $( "#toggle-button" ).click(function(e) {
+    e.preventDefault();
     $('#date-select-end').toggle();
     var checked = $('#date-range-checkbox')[0].checked;
     $('#date-range-checkbox')[0].checked = (checked) ? false : true;
   });
 
-  $(document).on('click', '.toggle-button', function() {
-    event.preventDefault();
+  $(document).on('click', '.toggle-button', function(e) {
+    e.preventDefault();
     $(this).toggleClass('toggle-button-selected'); 
     $('#date-select-end').toggle();
     var checked = $('#date-range-checkbox')[0].checked;
