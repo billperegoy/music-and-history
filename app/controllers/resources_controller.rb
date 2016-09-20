67cbdb0825558509be04d594e7f410e86d0b9377
Bill Peregoy <bill@billperegoy.com>
2016-09-20 12:58:51 -0400

    Fixed sort order for resources.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,6 @@
 class ResourcesController < ApplicationController
   def index
     @random_event = random_event_on_this_date_in_history
-    @resources = Resource.all
+    @resources = Resource.all.sort {|a,b| a.sort_order <=> b.sort_order}
   end
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,7 @@
 class Resource < ActiveRecord::Base
   validates :text, presence: true
+
+  def sort_order
+    text.downcase.gsub(/<[^>]*>/, '').gsub(/\"/, '')
+  end
 end
