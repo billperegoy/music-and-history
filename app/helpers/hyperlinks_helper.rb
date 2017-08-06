83dd060f9facf616605e8c197a04b6fa130cfcb2
Bill Peregoy <bill@peregoy.org>
2017-08-06 13:43:50 -0400

    Added srt_by field to hyperlinks

--- app/helpers/hyperlinks_helper.rb
+++ app/helpers/hyperlinks_helper.rb
@@ -1,5 +1,5 @@
 module HyperlinksHelper
   def sort_by_link_name(links)
-   links.sort_by { |item| item.name }
+   links.sort_by { |item| item.sort_by }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826005329) do
+ActiveRecord::Schema.define(version: 20170806170113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20160826005329) do
     t.integer  "link_category_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "sort_by"
   end
 
   create_table "link_categories", force: :cascade do |t|
